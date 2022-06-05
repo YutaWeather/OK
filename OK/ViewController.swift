e483f71f5a0570f783af4c8016fbf735d57a2a46
Yuta Fujii <yutayutakid21@gmail.com>
2022-06-05 17:14:40 +0900

    たちつてと

--- OK/ViewController.swift
+++ OK/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("かきくけこ")
         print("さしすせそ")
+        print("たちつてと")
         // Do any additional setup after loading the view.
     }
 
